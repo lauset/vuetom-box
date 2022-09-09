32fc31476f35fc0dc4e46f3df374c520172aa408
lauset <liujie_783@163.com>
2022-09-09 22:46:37 +0800

    主题配置

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import '@/assets/css/app.scss'
 import App from './App.vue'
 import { setupRouter } from '@/routes'
 import { setupLang } from '@/i18n'
+import { setupComs } from '@/components'
 
 const app = createApp(App)
 
 setupLang(app)
 setupRouter(app)
+setupComs(app)
 
 app.mount('#app')
