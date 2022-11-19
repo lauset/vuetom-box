58b2d2bf7edc578867fc8b403cab8bd7694535a2
lauset <liujie_783@163.com>
2022-11-19 16:15:15 +0800

    tauri: 依赖版本升级

--- src-tauri/src/winv.rs
+++ src-tauri/src/winv.rs
@@ -6,7 +6,7 @@ pub fn init(app: &mut App) -> std::result::Result<(), Box<dyn std::error::Error>
     let window = app.get_window("main").unwrap();
 
     #[cfg(target_os = "macos")]
-    apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow)
+    apply_vibrancy(&window, NSVisualEffectMaterial::AppearanceBased, None, None)
         .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
 
     #[cfg(target_os = "windows")]
